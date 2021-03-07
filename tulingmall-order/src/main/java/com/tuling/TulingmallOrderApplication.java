2cb17a6d58521d99b3aefe949f21c88d4773a0b6
monkey <2625273792@qq.com>
2021-03-07 15:32:14 +0800

    init

---
+++ tulingmall-order/src/main/java/com/tuling/TulingmallOrderApplication.java
@@ -0,0 +1,26 @@
+package com.tuling;
+
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.apache.shardingsphere.shardingjdbc.spring.boot.SpringBootConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(exclude = SpringBootConfiguration.class) /*(exclude = {GlobalTransactionAutoConfiguration.class})*/
+@EnableFeignClients
+public class TulingmallOrderApplication {
+
+	private static ApplicationContext applicationContext;
+
+	public static void main(String[] args) {
+		applicationContext = SpringApplication.run(TulingmallOrderApplication.class, args);
+	}
+
+	public static <T> T getBean(String beanName) {
+		return applicationContext != null ? (T) applicationContext.getBean(beanName) : null;
+	}
+
+
+}
